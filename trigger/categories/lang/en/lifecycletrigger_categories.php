e6190e89c30a52eb16b8c7840e12bea8688c685e
Justus Dieckmann <justusdieckmann@wwu.de>
2024-04-04 14:24:07 +0200

    Sort strings in lang files alphabetically

--- trigger/categories/lang/en/lifecycletrigger_categories.php
+++ trigger/categories/lang/en/lifecycletrigger_categories.php
@@ -22,9 +22,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Categories trigger';
-$string['privacy:metadata'] = 'This subplugin does not store any personal data.';
-
 $string['categories'] = 'Categories, for which the workflow should be triggered';
 $string['categories_noselection'] = 'Please choose at least one category.';
 $string['exclude'] = 'If ticked, the named categories are excluded from triggering instead.';
+$string['pluginname'] = 'Categories trigger';
+$string['privacy:metadata'] = 'This subplugin does not store any personal data.';
+
